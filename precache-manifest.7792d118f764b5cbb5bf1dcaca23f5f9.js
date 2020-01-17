6eab4a77f8f19131e9cdaf3dc5f69499d33932fe
sothearith <thearith6800@gmail.com>
2020-01-17 12:07:42 +0700

    manually

---
+++ precache-manifest.7792d118f764b5cbb5bf1dcaca23f5f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a0ea6a176aae281a1099d80424f696",
+    "url": "/testtGh-pages/index.html"
+  },
+  {
+    "revision": "3a9dc26bd1e8d5f32c74",
+    "url": "/testtGh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c88cebed7dec8a545eeb",
+    "url": "/testtGh-pages/static/js/2.250dc4af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testtGh-pages/static/js/2.250dc4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a9dc26bd1e8d5f32c74",
+    "url": "/testtGh-pages/static/js/main.d0368511.chunk.js"
+  },
+  {
+    "revision": "89692a98c59a9b3b96fb",
+    "url": "/testtGh-pages/static/js/runtime-main.499fb1ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testtGh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
